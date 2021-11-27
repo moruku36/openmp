45b2199079a3612b6a10d82bb55167d73606b78c
SeaOtter(Ken) <kentaro.mori36@gmail.com>
2021-11-27 14:56:30 +0900

    commit from mac os

--- omp_CYCLIC.c
+++ omp_CYCLIC.c
@@ -1,26 +1,62 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#ifdef _OPENMP
 #include <omp.h>
-#endif
+
 #define ITER (512*1024*1024)
 
-main(argc, argv )
-    int   argc;
-    char  *argv[];
+double parallel_rng(unsigned long long* seed)
 {
-        int numthreads, MyID;
-        int i, j, sum=0;
-        double x, y;
+    unsigned long long random = *seed;
 
-        #pragma omp parallel
-        numthreads = omp_get_num_threads();
+    random ^= random >> 12;
+    random ^= random << 25;
+    random ^= random >> 27;
 
-  for( i = 0; i < ITER; i++ ){
-    x = drand48();
-    y = drand48();
-    if(x*x+y*y < 1) sum++;
-  }
+    *seed = random;
 
-  printf( "PI=%f¥n", 4.0*sum/ITER );
+    return ((double) random * 0x2545F4914F6CDD1DULL) / ((double) 0xFFFFFFFFFFFFFFFFULL);
+}
+
+int main(int argc, char* argv[])
+{
+    int i;
+    int sum = 0;
+    int partial_sum;
+    unsigned long long seed;
+
+#pragma omp parallel private(partial_sum, seed)
+    {
+        partial_sum = 0;
+
+#pragma omp critical
+        {
+            seed = (unsigned long long) rand();
+            seed <<= 16;
+            seed += (unsigned long long) rand();
+            seed <<= 16;
+            seed += (unsigned long long) rand();
+            seed <<= 16;
+            seed += (unsigned long long) rand();
+        }
+
+#pragma omp for
+        for(i = 0; i < ITER; ++i)
+        {
+            double x = parallel_rng(&seed);
+            double y = parallel_rng(&seed);
+
+            if(x * x + y * y < 1.0)
+            {
+                ++partial_sum;
+            }
+        }
+
+#pragma omp critical
+        {
+            sum += partial_sum;
+        }
+    }
+
+    printf( "PI=%f¥n", 4.0*sum/ITER );
 }
